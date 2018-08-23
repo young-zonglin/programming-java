2a2435a47287952a75457948dbb559299f22b9d9
yangzonglin <806765278@qq.com>
2018-08-23 23:03:46 +0800

    Merge sort (from top to down) => done.

---
+++ src/com/yzl/sort/MergeSortFromTop.java
@@ -0,0 +1,41 @@
+package com.yzl.sort;
+
+public class MergeSortFromTop {
+    private static double[] aux;
+
+    private static void sort(double[] arr, int low, int high) {
+        if (low >= high) return;
+        int mid = low + (high-low)/2;
+        sort(arr, low, mid);
+        sort(arr, mid+1, high);
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(double[] arr, int low, int mid, int high) {
+        System.arraycopy(arr, low, aux, low, high-low+1);
+//        for (int k = low; k <= high; k++)
+//            aux[k] = arr[k];
+        int i = low;
+        int j = mid+1;
+        for (int k = low; k <= high; k++) {
+            if (i > mid) arr[k] = aux[j++];
+            else if (j > high) arr[k] = aux[i++];
+            else if (aux[i] < aux[j]) arr[k] = aux[i++];
+            else arr[k] = aux[j++];
+        }
+    }
+
+    public static double[] sort(double[] arr) {
+        aux = new double[arr.length];
+        sort(arr, 0, arr.length-1);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
+        arr = MergeSortFromTop.sort(arr);
+        for (double tmp : arr) {
+            System.out.print(tmp+" ");
+        }
+    }
+}
