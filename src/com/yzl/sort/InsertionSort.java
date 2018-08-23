363a1057248d79da4d3aac509975c03d640a7533
yangzonglin <806765278@qq.com>
2018-08-23 20:54:10 +0800

    Insertion sort => done.

---
+++ src/com/yzl/sort/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.yzl.sort;
+
+public class InsertionSort {
+    public static double[] sort(double[] arr) {
+        int len = arr.length;
+        for (int i=1; i<len; i++) {
+            double now = arr[i];
+            int j = i-1;
+            for (; j>=0; j--) {
+                if (arr[j] > now) {
+                    arr[j+1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j+1] = now;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
+        arr = InsertionSort.sort(arr);
+        for (double tmp : arr) {
+            System.out.print(tmp+" ");
+        }
+    }
+}
