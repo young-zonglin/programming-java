275a9c1b053fc41548278c791393030ca2f4f094
yangzonglin <806765278@qq.com>
2018-09-01 02:15:00 +0800

    Compute swap times for bubble sort.

--- src/yzl/sort/BubbleSort.java
+++ src/yzl/sort/BubbleSort.java
@@ -1,7 +1,10 @@
 package yzl.sort;
 
 public class BubbleSort {
+    public static int swap_count = 0;
+
     public static void sort(double[] arr) {
+        swap_count = 0;
         int len = arr.length;
         for (int i=1; i<=len-1; i++)
             for (int j=0; j<len-i; j++) {
@@ -9,15 +12,18 @@ public class BubbleSort {
                     double tmp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = tmp;
+                    swap_count++;
                 }
             }
     }
 
     public static void main(String[] args) {
-        double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7};
+//        double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7};
+        double[] arr = {5, 7, 0, 9, 2, 3, 1, 4};
         BubbleSort.sort(arr);
         for (double tmp : arr) {
             System.out.print(tmp+" ");
         }
+        System.out.println("\nswap times: " + BubbleSort.swap_count);
     }
 }
