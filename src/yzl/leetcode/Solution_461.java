cffc598f4bb1a141ca455fd1e05435481f8b0f98
yangzonglin <806765278@qq.com>
2018-09-04 20:52:04 +0800

    Add new solution to hamming distance problem.

--- src/yzl/leetcode/Solution_461.java
+++ src/yzl/leetcode/Solution_461.java
@@ -1,6 +1,13 @@
 package yzl.leetcode;
 
+/**
+ * Hamming distance.
+ */
 public class Solution_461 {
+
+}
+
+class Solution_461_one {
     public int hammingDistance(int x, int y) {
         int hammingDist = 0;
         for (int i = 0; i < 32; i++) {
@@ -11,5 +18,17 @@ public class Solution_461 {
             y >>= 1;
         }
         return hammingDist;
+    }
+}
+
+class Solution_461_two {
+    public int hammingDistance(int x, int y) {
+        int num = x ^ y;
+        int count = 0;
+        for (int i = 0; i<32; i++) {
+            count += num&1;
+            num >>= 1;
+        }
+        return count;
     }
 }
