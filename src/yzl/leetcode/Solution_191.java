971fbfd70642b5f82d5229d941dea937777f6f90
yangzonglin <806765278@qq.com>
2018-09-04 20:50:49 +0800

    Simplify code.

--- src/yzl/leetcode/Solution_191.java
+++ src/yzl/leetcode/Solution_191.java
@@ -8,9 +8,7 @@ public class Solution_191 {
     public int hammingWeight(int n) {
         int count = 0;
         for (int i = 0; i < 32; i++) {
-            if ((n & 1) == 1) {
-                count++;
-            }
+            count += n&1;
             n >>= 1;
         }
         return count;
