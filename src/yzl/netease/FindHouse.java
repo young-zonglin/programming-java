0e03c1fdfdd9303314f0542864a129e479f62556
yangzonglin <806765278@qq.com>
2018-09-08 18:08:19 +0800

    Netease coding test. Find house problem.

---
+++ src/yzl/netease/FindHouse.java
@@ -0,0 +1,24 @@
+package yzl.netease;
+
+import java.util.Scanner;
+
+public class FindHouse {
+    public int findMax(int n, int k) {
+        if (k <= 1 || k >= n) return 0;
+        int max;
+        if (k <= n/2) max = k-1;
+        else max = n-k;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int t = reader.nextInt();
+        for (int i = 0; i<t; i++) {
+            int n = reader.nextInt();
+            int k = reader.nextInt();
+            int max = new FindHouse().findMax(n, k);
+            System.out.println(0+" "+max);
+        }
+    }
+}
