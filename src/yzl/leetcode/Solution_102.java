0fe50fd60293f9c4bb173a81179a60866329eb9f
yangzonglin <806765278@qq.com>
2018-09-16 22:38:33 +0800

    Leetcode 102. BFS with queue.

---
+++ src/yzl/leetcode/Solution_102.java
@@ -0,0 +1,31 @@
+package yzl.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        } else {
+            deque.add(root);
+        }
+
+        while (!deque.isEmpty()) {
+            int width = deque.size();
+            List<Integer> layer = new ArrayList<>();
+            for (int i = 0; i<width; i++) {
+                TreeNode curNode = deque.removeFirst();
+                layer.add(curNode.val);
+                if (curNode.left != null) deque.add(curNode.left);
+                if (curNode.right != null) deque.add(curNode.right);
+            }
+            ret.add(layer);
+        }
+        return ret;
+    }
+}
