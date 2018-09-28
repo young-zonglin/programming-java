d0937c0409afb242dfd801f99026ae93ced2c780
yangzonglin <806765278@qq.com>
2018-09-28 22:05:41 +0800

    Modify dp solution of fibonacci. Change int to BigInteger.

--- src/yzl/fibonacci/DP.java
+++ src/yzl/fibonacci/DP.java
@@ -1,14 +1,15 @@
 package yzl.fibonacci;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DP {
-    public int fastFib(int n, Map<Integer, Integer> memory) {
-        if (n==0 || n==1) return 1;
-        Integer v = memory.get(n);
+    public BigInteger fastFib(int n, Map<Integer, BigInteger> memory) {
+        if (n==0 || n==1) return BigInteger.ONE;
+        BigInteger v = memory.get(n);
         if (v == null) {
-            int middle = fastFib(n-1, memory) + fastFib(n-2, memory);
+            BigInteger middle = fastFib(n-1, memory).add(fastFib(n-2, memory));
             memory.put(n, middle);
             return middle;
         } else {
@@ -18,9 +19,9 @@ public class DP {
 
     public static void main(String[] args) {
         int n = 150;
-        Map<Integer, Integer> memory = new HashMap<>();
+        Map<Integer, BigInteger> memory = new HashMap<>();
         Long start = System.currentTimeMillis();
-        int res = new DP().fastFib(n, memory);
+        BigInteger res = new DP().fastFib(n, memory);
         System.out.println(res);
         Long end = System.currentTimeMillis();
         System.out.println("Time consuming: "+(end-start)/1000.0);
