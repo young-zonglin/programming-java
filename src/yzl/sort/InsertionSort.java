25f8f270f25d2bb51b90de91c5066289cb307e10
yangzonglin <806765278@qq.com>
2022-09-08 22:44:18 +0800

    插入排序复习&&代码格式化

--- src/yzl/sort/InsertionSort.java
+++ src/yzl/sort/InsertionSort.java
@@ -1,15 +1,20 @@
 package yzl.sort;
 
+/**
+ * 插入排序
+ * 一个有序序列，将待排序元素，插入合适位置，涉及数组移动
+ * 升序排序
+ */
 public class InsertionSort {
     public static void sort(double[] arr) {
         int len = arr.length;
-        for (int i=1; i<len; i++) {
+        for (int i = 1; i < len; i++) {
             double now = arr[i];
-            int j = i-1;
-            for (; j>=0 && arr[j] > now; j--) {
-                arr[j+1] = arr[j];
+            int j = i - 1;
+            for (; j >= 0 && arr[j] > now; j--) {
+                arr[j + 1] = arr[j];
             }
-            arr[j+1] = now;
+            arr[j + 1] = now;
         }
     }
 
@@ -17,7 +22,7 @@ public class InsertionSort {
         double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
         InsertionSort.sort(arr);
         for (double tmp : arr) {
-            System.out.print(tmp+" ");
+            System.out.print(tmp + " ");
         }
     }
 }
