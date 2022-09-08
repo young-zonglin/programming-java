4dd9514527058730b095a5c96a5c5eafa224a47d
yangzonglin <806765278@qq.com>
2022-09-08 22:32:24 +0800

    选择排序复习&&代码格式化

--- src/yzl/sort/SelectionSort.java
+++ src/yzl/sort/SelectionSort.java
@@ -1,15 +1,21 @@
 package yzl.sort;
 
+/**
+ * 选择排序
+ * 升序排序
+ */
 public class SelectionSort {
     public static void sort(double[] arr) {
         int len = arr.length;
-        for (int i=0; i<=len-2; i++) {
+        for (int i = 0; i <= len - 2; i++) {
             int minIndex = i;
-            for (int j = i+1; j < len; j++) {
+            // 选择最小的
+            for (int j = i + 1; j < len; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
+            // 交换
             double tmp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = tmp;
@@ -20,7 +26,7 @@ public class SelectionSort {
         double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
         SelectionSort.sort(arr);
         for (double tmp : arr) {
-            System.out.print(tmp+" ");
+            System.out.print(tmp + " ");
         }
     }
 }
