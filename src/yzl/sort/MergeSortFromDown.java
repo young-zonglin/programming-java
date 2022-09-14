3f240df652f450a951a84c62d2ffccd8f2d76fac
yangzonglin <806765278@qq.com>
2022-09-14 22:14:40 +0800

    归并排序复习&&冗余代码抽取&&补齐注释

--- src/yzl/sort/MergeSortFromDown.java
+++ src/yzl/sort/MergeSortFromDown.java
@@ -1,36 +1,35 @@
 package yzl.sort;
+
 import java.lang.Math;
 
+/**
+ * 归并排序，非递归
+ */
 public class MergeSortFromDown {
-    private static double[] aux;
+    private static double[] aux; // 辅助空间
 
     private static void merge(double[] arr, int low, int mid, int high) {
-        System.arraycopy(arr, low, aux, low, high-low+1);
-        int i = low;
-        int j = mid+1;
-        for (int k = low; k <= high; k++) {
-            if (i > mid) arr[k] = aux[j++];
-            else if (j > high) arr[k] = aux[i++];
-            // For stable sorting.
-            else if (aux[i] <= aux[j]) arr[k] = aux[i++];
-            else arr[k] = aux[j++];
-        }
+        MergeSortFromTop.merge(arr, low, mid, high, aux);
     }
 
     public static void sort(double[] arr) {
         int len = arr.length;
         aux = new double[len];
-        for (int sz = 1; sz<len; sz *= 2)
-            // mid < len-1 => low < len-sz
-            for (int low = 0; low < len-sz; low += sz*2)
-                merge(arr, low, low+sz-1, Math.min(low+2*sz-1, len-1));
+        // sz是步长，逐步变大，一一 => 两两 => 四四
+        for (int sz = 1; sz < len; sz *= 2) {
+            // 开始两两归并
+            // mid < len-1 并且 mid=low + sz - 1 => low < len-sz
+            for (int low = 0; low < len - sz; low += sz * 2) {
+                merge(arr, low, low + sz - 1, Math.min(low + 2 * sz - 1, len - 1));
+            }
+        }
     }
 
     public static void main(String[] args) {
         double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
         MergeSortFromDown.sort(arr);
         for (double tmp : arr) {
-            System.out.print(tmp+" ");
+            System.out.print(tmp + " ");
         }
     }
 }
--- src/yzl/sort/MergeSortFromTop.java
+++ src/yzl/sort/MergeSortFromTop.java
@@ -1,32 +1,47 @@
 package yzl.sort;
 
+/**
+ * 归并排序，分治的思想
+ * 升序排序
+ * 递归
+ */
 public class MergeSortFromTop {
-    private static double[] aux;
+    private static double[] aux; // 辅助数组，归并排序占用额外的空间
 
     public static void sort(double[] arr) {
         aux = new double[arr.length];
-        sort(arr, 0, arr.length-1);
+        sort(arr, 0, arr.length - 1);
     }
 
     private static void sort(double[] arr, int low, int high) {
         if (low >= high) return;
-        int mid = low + (high-low)/2;
+        int mid = low + (high - low) / 2; // 二分，重要公式
         sort(arr, low, mid);
-        sort(arr, mid+1, high);
+        sort(arr, mid + 1, high);
         merge(arr, low, mid, high);
     }
 
     private static void merge(double[] arr, int low, int mid, int high) {
-        System.arraycopy(arr, low, aux, low, high-low+1);
-//        for (int k = low; k <= high; k++)
-//            aux[k] = arr[k];
+        merge(arr, low, mid, high, aux);
+    }
+
+    static void merge(double[] array, int low, int mid, int high, double[] aux) {
+        System.arraycopy(array, low, aux, low, high - low + 1);
         int i = low;
-        int j = mid+1;
+        int j = mid + 1;
         for (int k = low; k <= high; k++) {
-            if (i > mid) arr[k] = aux[j++];
-            else if (j > high) arr[k] = aux[i++];
-            else if (aux[i] <= aux[j]) arr[k] = aux[i++];
-            else arr[k] = aux[j++];
+            if (i > mid) { // 左数组完了
+                array[k] = aux[j++];
+            } else if (j > high) { // 右数组完了
+                array[k] = aux[i++];
+            } else if (aux[i] <= aux[j]) {
+                // For stable sorting.
+                // 双指针
+                // 升序排序
+                array[k] = aux[i++];
+            } else {
+                array[k] = aux[j++];
+            }
         }
     }
 
@@ -34,7 +49,7 @@ public class MergeSortFromTop {
         double[] arr = {1, 5.2, 3, 4, 10, 7, 1, 3, 5, 7, -1, -5, 0, 10, 5.8};
         MergeSortFromTop.sort(arr);
         for (double tmp : arr) {
-            System.out.print(tmp+" ");
+            System.out.print(tmp + " ");
         }
     }
 }
