7dd6f2e03d771a036b8958637487172c44791756
yangzonglin <806765278@qq.com>
2022-09-14 23:50:56 +0800

    复习基于非递归的快排，这种交换方式

--- src/yzl/sort/QuickSortByStack.java
+++ src/yzl/sort/QuickSortByStack.java
@@ -5,31 +5,36 @@ import java.util.Stack;
 
 public class QuickSortByStack {
     public static void main(String[] args) {
-        int[] arr = {6,1,3,9,6,4};
-        sort(arr, 0, arr.length-1);
+        int[] arr = {11, 11, 6, 1, 3, 9, 6, 4, 9, 2};
+        sort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
+    /*
+     * 本质上是一个BFS
+     * 基于Stack栈
+     * 使用栈来保存区间内的起始和结束指针
+     */
     public static void sort(int[] arr, int low, int high) {
-        // 使用栈来保存区间内的起始和结束指针
         Stack<Integer> stack = new Stack<>();
         if (low < high) {
+            // stack的初始化
             stack.push(high);
             stack.push(low);
             while (!stack.isEmpty()) {
                 int left = stack.pop();
                 int right = stack.pop();
-                // 每次通过partition函数来确定子区间的结束或起始位置
+                // 每次通过partition函数来确定pivot的位置
                 int idx = partition(arr, left, right);
                 // 左区间
-                if (left < idx-1) {
-                    stack.push(idx-1);
+                if (left < idx - 1) {
+                    stack.push(idx - 1);
                     stack.push(left);
                 }
                 // 右区间
-                if (right > idx+1) {
+                if (right > idx + 1) {
                     stack.push(right);
-                    stack.push(idx+1);
+                    stack.push(idx + 1);
                 }
             }
         }
