8eb1a8d61c6d79dc70a43c30fa0d0a1bc167858e
yangzonglin <806765278@qq.com>
2022-09-20 00:12:56 +0800

    代码格式化 && 一点小优化

--- src/yzl/leetcode/Solution_1.java
+++ src/yzl/leetcode/Solution_1.java
@@ -7,42 +7,10 @@ import java.util.*;
  */
 public class Solution_1 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,4,2,5};
-        int[] res = new Solution_1_wrong().twoSum(arr, 6);
+        int[] arr = new int[]{1, 2, 4, 2, 5};
+//        int[] res = new Solution_1_wrong().twoSum(arr, 6);
+        int[] res = new Solution_1_three().twoSum(arr, 6);
         System.out.println(Arrays.toString(res));
-    }
-}
-
-/**
- * Complicated the problem...
- */
-class Solution_1_one {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> numToIdxs = new HashMap<>();
-        int i, end=1;
-        List<Integer> idxs;
-        for (i = 0; i<nums.length; i++) {
-            idxs = numToIdxs.get(nums[i]);
-            if (idxs == null) {
-                idxs = new ArrayList<>();
-                idxs.add(i);
-                numToIdxs.put(nums[i], idxs);
-            } else {
-                idxs.add(i);
-            }
-        }
-        for (i = 0; i<nums.length; i++) {
-            if ((idxs=numToIdxs.get(target-nums[i])) != null) {
-                for (int tmp : idxs) {
-                    if (tmp != i) {
-                        end = tmp;
-                        return new int[] {i, end};
-                    }
-                }
-            }
-        }
-
-        return new int[] {i, end};
     }
 }
 
@@ -53,13 +21,13 @@ class Solution_1_two {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numToIdx = new HashMap<>();
         int i, end;
-        for (i = 0; i<nums.length; i++) {
+        for (i = 0; i < nums.length; i++) {
             numToIdx.put(nums[i], i);
         }
-        for (i = 0; i<nums.length; i++) {
-            int complement = target-nums[i];
-            if (numToIdx.containsKey(complement) && (end=numToIdx.get(complement)) != i) {
-                return new int[] {i, end};
+        for (i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIdx.containsKey(complement) && (end = numToIdx.get(complement)) != i) {
+                return new int[]{i, end};
             }
         }
         throw new IllegalArgumentException("No two sum solution.");
@@ -71,24 +39,25 @@ class Solution_1_two {
  */
 class Solution_1_three {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i<nums.length; i++) {
-            int complement = target-nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
+        Map<Integer, Integer> cache = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (cache.containsKey(complement)) {
+                return new int[]{cache.get(complement), i};
+            } else {
+                cache.put(nums[i], i);
             }
-            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution.");
+        return new int[]{-1, -1};
     }
 }
 
 class Solution_1_wrong {
     public int[] twoSum(int[] nums, int target) {
         Arrays.sort(nums);
-        int i, j=1;
-        for (i = 0; i < nums.length-1; i++) {
-            j = binarySearch(nums, i+1, target-nums[i]);
+        int i, j = 1;
+        for (i = 0; i < nums.length - 1; i++) {
+            j = binarySearch(nums, i + 1, target - nums[i]);
             if (j >= 0) break;
         }
         if (j < 0)
@@ -100,12 +69,12 @@ class Solution_1_wrong {
         int e = nums.length - 1;
         int mid, midVal;
         while (s <= e) {
-            mid = s+(e-s)/2;
+            mid = s + (e - s) / 2;
             midVal = nums[mid];
             if (midVal == key) return mid;
-            else if (midVal < key) s=mid+1;
-            else e=mid-1;
+            else if (midVal < key) s = mid + 1;
+            else e = mid - 1;
         }
-        return -(s+1);
+        return -(s + 1);
     }
 }
