95b4c859953e3de1ba008c64555a5d949930c9b4
yangzonglin <806765278@qq.com>
2022-10-12 22:54:48 +0800

    补齐注释

--- src/yzl/median/QuickSelect.java
+++ src/yzl/median/QuickSelect.java
@@ -7,14 +7,29 @@ import yzl.utils.Tools;
  * Find k-th element.
  */
 public class QuickSelect {
+    /**
+     * 基于二分和快排Partition
+     *
+     * @param arr 原始数组
+     * @param k   因为从0算起，其实是top K+1
+     * @return top k-th 元素
+     */
     public static double select(Double[] arr, int k) {
         int low = 0;
-        int high = arr.length-1;
+        int high = arr.length - 1;
         while (low < high) {
-            int j = QuickSort.partition(arr, low, high);
-            if (j == k) return arr[k];
-            else if (j < k) low = j+1;
-            else if (j > k) high = j-1;
+            // 小 < pivot < 大
+            int pivot = QuickSort.partition(arr, low, high);
+            // 二分搜索
+            if (pivot == k) {
+                return arr[k];
+            } else if (pivot < k) {
+                // 搜索右边
+                low = pivot + 1;
+            } else {
+                // 搜索左边
+                high = pivot - 1;
+            }
         }
         return arr[k];
     }
