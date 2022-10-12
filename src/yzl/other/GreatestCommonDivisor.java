65ceb5188e4bcaf37597088baa2e00935204cb4f
yangzonglin <806765278@qq.com>
2022-10-12 23:11:44 +0800

    补齐注释

--- src/yzl/other/GreatestCommonDivisor.java
+++ src/yzl/other/GreatestCommonDivisor.java
@@ -2,11 +2,15 @@ package yzl.other;
 
 import java.util.Scanner;
 
+/**
+ * 更相减损术， 出自于中国古代的《九章算术》
+ * 更相减损术 优于 辗转相除法
+ */
 public class GreatestCommonDivisor {
     public int getGCD(int a, int b) {
         if (a == b) return a;
-        else if (a > b) return getGCD(a-b, b);
-        else return getGCD(a, b-a);
+        else if (a > b) return getGCD(a - b, b);
+        else return getGCD(b - a, a);
     }
 
     public static void main(String[] args) {
