da9a4d5d60040a7f626b84501a4e79a5524a82d3
yangzonglin <806765278@qq.com>
2022-10-12 22:18:13 +0800

    补齐注释

--- src/yzl/topk/TopKMinHeap.java
+++ src/yzl/topk/TopKMinHeap.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class TopKMinHeap {
     public static void main(String[] args) {
-        int[] nums = {1,8,20,3,5,41,2,1,1,5,7,20,90,50,3};
+        int[] nums = {1, 8, 20, 3, 5, 41, 2, 1, 1, 5, 7, 20, 90, 50, 3};
         int k = 10;
         int[] res = TopKMinHeap.topK(nums, k);
         System.out.println(Arrays.toString(res));
@@ -14,11 +14,12 @@ public class TopKMinHeap {
         int[] ret = new int[k];
         System.arraycopy(nums, 0, ret, 0, k);
         // initial min heap
-        for (int i = k/2-1; i>=0; i--) {
+        for (int i = k / 2 - 1; i >= 0; i--) {
             adjustMinHeap(ret, i, k);
         }
         // top k
-        for (int i = k; i<nums.length; i++) {
+        // 剩下的数，如果大于堆顶，则放入小顶堆，并且调整小顶堆
+        for (int i = k; i < nums.length; i++) {
             if (nums[i] > ret[0]) {
                 ret[0] = nums[i];
                 adjustMinHeap(ret, 0, k);
@@ -28,9 +29,12 @@ public class TopKMinHeap {
     }
 
     public static void adjustMinHeap(int[] nums, int min, int len) {
+        // 只交换一次，而不是一直在交换
         int tmp = nums[min];
-        for (int k = 2*min+1; k<len; k = 2*k+1) {
-            if (k+1 < len && nums[k]>nums[k+1]) k++;
+        for (int k = 2 * min + 1; k < len; k = 2 * k + 1) {
+            if (k + 1 < len && nums[k] > nums[k + 1]) {
+                k++;
+            }
             if (nums[k] < tmp) {
                 nums[min] = nums[k];
                 min = k;
