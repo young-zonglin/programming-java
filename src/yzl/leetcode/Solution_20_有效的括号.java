3e18bc8481252b262a47f6def7e360991fb001b8
yangzonglin <806765278@qq.com>
2024-03-09 18:56:52 +0800

    提交1版，耗时只击败8%的Java用户

---
+++ src/yzl/leetcode/Solution_20_有效的括号.java
@@ -0,0 +1,42 @@
+package yzl.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
+
+public class Solution_20_有效的括号 {
+    public boolean isValid(String s) {
+        List<Character> left = Arrays.asList('(', '{', '[');
+        Map<Character, Integer> char2match = new HashMap<Character, Integer>() {{
+            put('(', 1);
+            put(')', 1);
+            put('{', 2);
+            put('}', 2);
+            put('[', 3);
+            put(']', 3);
+        }};
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (left.contains(c)) {
+                stack.push(c);
+            } else {
+                if (stack.empty()) return false;
+                Character tmp = stack.pop();
+                if (!Objects.equals(char2match.get(tmp), char2match.get(c))) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_20_有效的括号().isValid("]"));
+        System.out.println(new Solution_20_有效的括号().isValid("(]"));
+        System.out.println(new Solution_20_有效的括号().isValid("["));
+        System.out.println(new Solution_20_有效的括号().isValid("[]"));
+    }
+}
