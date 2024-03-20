8f639403fdb1ac32c18add84b0d26610c3c48865
yangzonglin <806765278@qq.com>
2024-03-20 23:15:51 +0800

    使用滑动窗口

--- src/yzl/leetcode/Solution_438_找到字符串中所有字母异位词.java
+++ src/yzl/leetcode/Solution_438_找到字符串中所有字母异位词.java
@@ -35,6 +35,33 @@ public class Solution_438_找到字符串中所有字母异位词 {
 
 class Solution_438_滑动窗口 {
     public List<Integer> findAnagrams(String s, String p) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return result;
+        }
+        int[] window = new int[26];
+        int[] target = new int[26];
+        for (char c : p.toCharArray()) {
+            target[c - 'a']++;
+        }
+        for (int i = 0; i < p.length(); i++) {
+            window[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(target, window)) {
+            result.add(0);
+        }
+        for (int left = 0, right = p.length() - 1; right < s.length() - 1; left++, right++) {
+            window[s.charAt(left) - 'a']--;
+            window[s.charAt(right + 1) - 'a']++;
+            if (Arrays.equals(target, window)) {
+                result.add(left + 1);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd", p = "abc";
+        System.out.println(new Solution_438_滑动窗口().findAnagrams(s, p));
     }
 }
