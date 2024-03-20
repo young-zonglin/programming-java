156b0efb9588d7ff908cbffe848b06f6a4607763
yangzonglin <806765278@qq.com>
2024-03-20 23:39:57 +0800

    使用链表可以巧妙解决

---
+++ src/yzl/leetcode/Solution_155_最小栈.java
@@ -0,0 +1,42 @@
+package yzl.leetcode;
+
+public class Solution_155_最小栈 {
+    Node head = null;
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min) {
+            this.val = val;
+            this.min = min;
+        }
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
