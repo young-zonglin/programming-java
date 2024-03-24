b41728e9a2664c6bcbb53cbc3cf3394bb7c968c9
yangzonglin <806765278@qq.com>
2024-03-24 17:47:50 +0800

    HashMap，简单题

---
+++ src/yzl/leetcode/Solution_383_赎金信_简单.java
@@ -0,0 +1,31 @@
+package yzl.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_383_赎金信_简单 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (map.containsKey(c)) {
+                int cnt = map.get(c);
+                if (cnt > 0) {
+                    cnt--;
+                    map.put(c, cnt);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
