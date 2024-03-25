44eb81e19bfb067d817b05385eed1bd2519b00be
yangzonglin <806765278@qq.com>
2024-03-25 23:11:05 +0800

    递归解法，翻转二叉树

---
+++ src/yzl/leetcode/Solution_226_翻转二叉树.java
@@ -0,0 +1,15 @@
+package yzl.leetcode;
+
+public class Solution_226_翻转二叉树 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
