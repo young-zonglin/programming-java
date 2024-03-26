8969278ef365ce3813e5a61e9a05d65123e2ec9b
yangzonglin <806765278@qq.com>
2024-03-26 23:36:43 +0800

    递归解法，两棵二叉树是否相同

---
+++ src/yzl/leetcode/Solution_100_相同的树.java
@@ -0,0 +1,13 @@
+package yzl.leetcode;
+
+public class Solution_100_相同的树 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
