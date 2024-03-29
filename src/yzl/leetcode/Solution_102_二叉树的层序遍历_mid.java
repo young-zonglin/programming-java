84a9289ab028acf2d9855e18ef6cb381fb685908
yangzonglin <806765278@qq.com>
2024-03-29 23:46:02 +0800

    使用队列来层序遍历二叉树

--- src/yzl/leetcode/Solution_102_二叉树的层序遍历_mid.java
+++ src/yzl/leetcode/Solution_102_二叉树的层序遍历_mid.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-public class Solution_102 {
+public class Solution_102_二叉树的层序遍历_mid {
     public List<List<Integer>> levelOrder(TreeNode root) {
         Deque<TreeNode> deque = new ArrayDeque<>();
         List<List<Integer>> ret = new ArrayList<>();
@@ -18,7 +18,7 @@ public class Solution_102 {
         while (!deque.isEmpty()) {
             int width = deque.size();
             List<Integer> layer = new ArrayList<>();
-            for (int i = 0; i<width; i++) {
+            for (int i = 0; i < width; i++) {
                 TreeNode curNode = deque.removeFirst();
                 layer.add(curNode.val);
                 if (curNode.left != null) deque.add(curNode.left);
