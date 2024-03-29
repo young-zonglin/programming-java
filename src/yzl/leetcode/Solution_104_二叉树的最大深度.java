c1e30e70b1692def422ed0615e171ca27eb2e945
yangzonglin <806765278@qq.com>
2024-03-30 00:03:00 +0800

    新增非递归解法解决最大深度，通过层级遍历

--- src/yzl/leetcode/Solution_104_二叉树的最大深度.java
+++ src/yzl/leetcode/Solution_104_二叉树的最大深度.java
@@ -1,7 +1,32 @@
 package yzl.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution_104_二叉树的最大深度 {
     public int maxDepth(TreeNode root) {
         return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+
+class Solution_104_非递归解法 {
+    public int maxDepth(TreeNode root) {
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        if (root != null) {
+            deque.add(root);
+        } else {
+            return 0;
+        }
+        int maxDepth = 0;
+        while (!deque.isEmpty()) {
+            maxDepth++;
+            int width = deque.size();
+            for (int i = 0; i < width; i++) {
+                TreeNode curNode = deque.removeFirst();
+                if (curNode.left != null) deque.add(curNode.left);
+                if (curNode.right != null) deque.add(curNode.right);
+            }
+        }
+        return maxDepth;
     }
 }
