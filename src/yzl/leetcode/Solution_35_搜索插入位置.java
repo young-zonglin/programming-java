35caf8881ac7c42bf57437551d124dca28e5129b
yangzonglin <806765278@qq.com>
2024-04-07 23:37:08 +0800

    使用二分法

--- src/yzl/leetcode/Solution_35_搜索插入位置.java
+++ src/yzl/leetcode/Solution_35_搜索插入位置.java
@@ -8,3 +8,22 @@ public class Solution_35_搜索插入位置 {
         return nums.length;
     }
 }
+
+class Solution_35_binSearch {
+    public int searchInsert(int[] nums, int target) {
+        int begin = 0, end = nums.length;
+        while (begin < end) {
+            int mid = begin + (end - begin) / 2;
+            int midVal = nums[mid];
+            if (midVal == target) return mid;
+            else if (midVal < target) begin = mid + 1;
+            else end = mid;
+        }
+        return begin;
+    }
+
+    public static void main(String[] args) {
+        int pos = new Solution_35_binSearch().searchInsert(new int[]{1, 3, 5, 6}, 2);
+        System.out.println(pos);
+    }
+}
