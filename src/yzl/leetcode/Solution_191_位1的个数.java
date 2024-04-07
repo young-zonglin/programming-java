d762ebfeadb8a53b7d0cf86158f7b6c19f506fd3
yangzonglin <806765278@qq.com>
2024-04-08 00:45:58 +0800

    新增一种解法，微小的调整

--- src/yzl/leetcode/Solution_191_位1的个数.java
+++ src/yzl/leetcode/Solution_191_位1的个数.java
@@ -3,13 +3,21 @@ package yzl.leetcode;
 /**
  * Number of 1 bits.
  */
-public class Solution_191 {
+public class Solution_191_位1的个数 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
         for (int i = 0; i < 32; i++) {
-            count += n&1;
+            count += n & 1;
             n >>= 1;
+        }
+        return count;
+    }
+
+    public int hammingWeight1(int n) {
+        int count = 0;
+        for (int i = 1; i <= 32; i++) {
+            count += n >> i & 1;
         }
         return count;
     }
