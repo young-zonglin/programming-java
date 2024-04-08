3643e32a4d0d538542a6d64e39a13658c6adb336
yangzonglin <806765278@qq.com>
2024-04-09 02:13:11 +0800

    修改类名 && 使用更好性能的sb

--- src/yzl/leetcode/Solution_125_验证回文串.java
+++ src/yzl/leetcode/Solution_125_验证回文串.java
@@ -3,15 +3,15 @@ package yzl.leetcode;
 /**
  * Valid palindrome string.
  */
-public class Solution_125 {
+public class Solution_125_验证回文串 {
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        System.out.println(new Solution_125().isPalindrome(s));
+        System.out.println(new Solution_125_验证回文串().isPalindrome(s));
     }
 
     public boolean isPalindrome(String s) {
         String forward = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        String backward = new StringBuffer(forward).reverse().toString();
+        String backward = new StringBuilder(forward).reverse().toString();
         return forward.equals(backward);
     }
 }
