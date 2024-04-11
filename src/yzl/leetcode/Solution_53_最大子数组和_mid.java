ad67bacbe50b21499868b8c89de72829d895f883
yangzonglin <806765278@qq.com>
2024-04-12 02:02:27 +0800

    动态规划解法

--- src/yzl/leetcode/Solution_53_最大子数组和_mid.java
+++ src/yzl/leetcode/Solution_53_最大子数组和_mid.java
@@ -1,5 +1,7 @@
 package yzl.leetcode;
 
+import java.util.Arrays;
+
 public class Solution_53_最大子数组和_mid {
     // 前缀和解法，会超时。。
     public int maxSubArray_timeout(int[] nums) {
@@ -17,9 +19,19 @@ public class Solution_53_最大子数组和_mid {
         return max;
     }
 
+    public int maxSubArray_dp1(int[] nums) {
+        // dp[i]：以i为结尾的最大子数组和，或者说最大连续子序列和
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+        }
+        return Arrays.stream(nums).max().orElse(-1);
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int max = new Solution_53_最大子数组和_mid().maxSubArray_timeout(nums);
+        int max1 = new Solution_53_最大子数组和_mid().maxSubArray_dp1(nums);
         System.out.println("前缀和：" + max);
+        System.out.println("dp1：" + max1);
     }
 }
