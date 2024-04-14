e3edb65e68fe289f889dcd6e303a9c668829b482
yangzonglin <806765278@qq.com>
2024-04-14 11:21:56 +0800

    补上另外两种方法的注释

--- src/yzl/leetcode/Solution_73_矩阵置零.java
+++ src/yzl/leetcode/Solution_73_矩阵置零.java
@@ -3,6 +3,11 @@ package yzl.leetcode;
 import java.util.Stack;
 
 public class Solution_73_矩阵置零 {
+    /*
+     * 方法1：使用O(m*n)的额外空间，比如用stack
+     * 方法2：使用O(m+n)的额外空间，比如用line[m]存储哪些行要改为0，用column[n]存储哪些列要改为0
+     * 方法3：使用常量空间，其实就是复用matrix入参的第一行和第一列。具体可参考LeetCode题解。
+     */
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
