c23aaf67e72e752c40b92c6cd31366372da8af7b
yangzonglin <806765278@qq.com>
2024-04-17 23:17:51 +0800

    模拟法：先按对角线翻一下，再逐行逆序

---
+++ src/yzl/leetcode/Solution_48_旋转图像_mid.java
@@ -0,0 +1,24 @@
+package yzl.leetcode;
+
+public class Solution_48_旋转图像_mid {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 先按对角线翻一下
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        // 逐行逆序
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+    }
+}
