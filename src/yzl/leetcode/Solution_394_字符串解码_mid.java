938981c08135270c1f7248bb785f8a5275dc60c1
yangzonglin <806765278@qq.com>
2024-05-01 00:54:06 +0800

    优化变量名等

--- src/yzl/leetcode/Solution_394_字符串解码_mid.java
+++ src/yzl/leetcode/Solution_394_字符串解码_mid.java
@@ -3,29 +3,39 @@ package yzl.leetcode;
 import java.util.Stack;
 
 public class Solution_394_字符串解码_mid {
+    /*
+     * 比如acd32[aa]aa2[a]
+     * 1. 遇到[，就把"acd"和32入栈
+     * 2. 遇到]，就出栈，并且acd+32*aa
+     */
     public String decodeString(String s) {
         Stack<String[]> stack = new Stack<>();
         int num = 0;
-        String tmp = "";
+        String res = "";
         for (int i = 0; i < s.length(); i++) {
             String cur = s.substring(i, i + 1);
             if (cur.matches("\\d+")) {
-                num = Integer.parseInt(cur) + 10 * num;
+                num = 10 * num + Integer.parseInt(cur);
             } else if (cur.equals("[")) {
-                stack.push(new String[]{tmp, String.valueOf(num)});
+                stack.push(new String[]{res, String.valueOf(num)});
                 num = 0;
-                tmp = "";
+                res = "";
             } else if ("]".equals(cur)) {
                 String[] arr = stack.pop();
-                String tmp1 = arr[0];
-                int num1 = Integer.parseInt(arr[1]);
-                String tmp22 = "";
-                for (int j = 0; j < num1; j++) tmp22 += tmp;
-                tmp = tmp1 + tmp22;
+                StringBuilder timesStr = new StringBuilder();
+                int times = Integer.parseInt(arr[1]);
+                for (int j = 0; j < times; j++) timesStr.append(res);
+                res = arr[0] + timesStr;
             } else {
-                tmp += cur;
+                res += cur;
             }
         }
-        return tmp;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String encoded = "acd5[db]ae2[ca]";
+        String ans = new Solution_394_字符串解码_mid().decodeString(encoded);
+        System.out.println(ans);
     }
 }
