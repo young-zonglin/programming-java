4a6f0c1b811e575b19c5bb73f2b58c38e1cfd3e8
yangzonglin <806765278@qq.com>
2024-05-05 14:48:45 +0800

    补充一些学习笔记

--- src/yzl/leetcode/Solution_17_电话号码的字母组合_mid.java
+++ src/yzl/leetcode/Solution_17_电话号码的字母组合_mid.java
@@ -25,17 +25,20 @@ public class Solution_17_电话号码的字母组合_mid {
      * 2、子问题：dfs(i)
      * 3、下一个子问题：dfs(i+1)
      *
-     * 时间复杂度：O(n*4^n)，
-     * 有些数字可以映射4个字母。
+     * 时间复杂度：O(4^n * n)
+     * 有些数字可以映射4个字母 => 4^n
      * 最后添加答案的时候，要把数组变成字符串，这里有n的时间复杂度
      */
     private void dfs(int i, int n, String digits, List<String> ans) {
+        // 边界条件
         if (i == n) {
+            // 需要复制到ans里
             ans.add(String.join("", path));
             return;
         }
         String curEnum = mapping[Integer.parseInt(digits.substring(i, i + 1))];
         for (int j = 0; j < curEnum.length(); j++) {
+            // 这里每次覆盖path[i]，所以其实也是有回溯的
             path[i] = curEnum.substring(j, j + 1);
             dfs(i + 1, n, digits, ans);
         }
