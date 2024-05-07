62daa884371924696dc499a98a1ca671676ccfe9
yangzonglin <806765278@qq.com>
2024-05-07 23:33:10 +0800

    贪心解法，其实就是避开为0的格子就可以

--- src/yzl/leetcode/Solution_55_跳跃游戏_mid.java
+++ src/yzl/leetcode/Solution_55_跳跃游戏_mid.java
@@ -1,6 +1,20 @@
 package yzl.leetcode;
 
 public class Solution_55_跳跃游戏_mid {
+    /*
+     * 只要能够避开为0的格子，就一定能走到数组的尾巴
+     */
+    public boolean canJump(int[] nums) {
+        // 先记录第一个下标的跳跃能力，即能走到哪个下标
+        int runToPos = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 当前格子能走到哪里
+            int curToPos = nums[i] + i;
+            // runToPos>=i，才意味着，可以走到当前的格子
+            if (runToPos >= i && curToPos > runToPos) runToPos = curToPos;
+        }
+        return runToPos >= nums.length - 1; //判断最终能否抵达最后一个索引
+    }
 }
 
 class Solution_55_timeout {
